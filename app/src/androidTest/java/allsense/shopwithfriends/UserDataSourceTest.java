d821d582405d63c5067149c49128367535daa165
aeubanks <arthur@creubank.com>
2015-03-31 19:54:23 -0400

    updated UserDataSourceTest

--- app/src/androidTest/java/allsense/shopwithfriends/UserDataSourceTest.java
+++ app/src/androidTest/java/allsense/shopwithfriends/UserDataSourceTest.java
@@ -7,6 +7,7 @@ public class UserDataSourceTest extends ActivityInstrumentationTestCase2<Welcome
     UserDataSource source;
     User user1;
     User user2;
+    User user3;
 
     public UserDataSourceTest() {
         super(WelcomeActivity.class);
@@ -19,10 +20,11 @@ public class UserDataSourceTest extends ActivityInstrumentationTestCase2<Welcome
         source = new UserDataSource(getActivity().getApplicationContext());
         user1 = User.addUser("name1", "email1", "user1", "pass1");
         user2 = User.addUser("name2", "email2", "user2", "pass2");
+        user3 = User.addUser("name3", "email3", "user3", "pass3");
     }
 
     // Arthur Eubanks
-    public void testAddFriends() {
+    public void testOneAddFriends() {
         assertFalse(user1.friends().contains(user2));
         assertFalse(user2.friends().contains(user1));
         source.addFriends(user1, user2);
@@ -30,17 +32,36 @@ public class UserDataSourceTest extends ActivityInstrumentationTestCase2<Welcome
         assertTrue(user2.friends().contains(user1));
     }
 
-    public void testRating() {
-        user1.rate(user2, 4);
-//        source.rate(user1, user2, 4);
-        assertEquals(0, user1.rating());
-        assertEquals(4, user2.rating());
+    public void testMultipleAddFriends() {
+        User[] users = {user1, user2, user3};
+
+        for (User u1 : users) {
+            for (User u2 : users) {
+                assertFalse(u1.friends().contains(u2));
+            }
+        }
+
+        user1.addFriend(user2);
+        user2.addFriend(user3);
+
+        assertTrue(user1.friends().contains(user2));
+        assertTrue(user2.friends().contains(user3));
+        assertFalse(user1.friends().contains(user3));
+
+        user1.addFriend(user3);
+
+        for (User u1 : users) {
+            for (User u2 : users) {
+                assertEquals(u1 != u2, u1.friends().contains(u2));
+            }
+        }
     }
 
     @Override
     protected void tearDown() throws Exception {
         source.deleteUser(user1);
         source.deleteUser(user2);
+        source.deleteUser(user3);
 
         super.tearDown();
     }
